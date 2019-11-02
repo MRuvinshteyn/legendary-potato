24cc780bb678ff8c7ad828bd1927c44a4098cdc5
Michael Ruvinshteyn <michael.ruvinshteyn@gmail.com>
2019-11-02 19:55:20 -0400

    added redirect to login

--- legendarypotato/__init__.py
+++ legendarypotato/__init__.py
@@ -3,18 +3,32 @@ from functools import wraps
 import os
 import random
 
+# authentication wrapper
+def require_login(f):
+    @wraps(f)
+    def inner(*args, **kwargs):
+        if 'user' not in session:
+            flash("Please log in to use Legendary Potato")
+            return redirect(url_for("login"))
+        else:
+            return f(*args, **kwargs)
+    return inner
+
 app = Flask(__name__)
 app.secret_key = os.urandom(16)
 
 @app.route("/")
+@require_login
 def root():
     return render_template("home.html")
 
 @app.route("/single")
+@require_login
 def single():
     return render_template("single.html")
 
 @app.route("/multi")
+@require_login
 def multi():
     return render_template("multi.html")
 
