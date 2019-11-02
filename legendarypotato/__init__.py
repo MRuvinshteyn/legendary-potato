6f39ef89c2415e3cb8135f11c2d84c7b9ebe7db4
Jake Zaia <jlz2369@g.rit.edu>
2019-11-02 16:47:24 -0400

    Push base repo

---
+++ legendarypotato/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from functools import wraps
+import os
+import random
+from utils import cloudFunctions
+from utils import mongoUtils
+
+app = Flask(__name__)
+app.secret_key = os.urandom(16)
+
+@app.route("/")
+def root():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host="0.0.0.0")
