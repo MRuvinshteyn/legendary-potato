99854769935e783d0ff8c3f36c0555ca45deb199
Jake Zaia <jlz2369@g.rit.edu>
2019-11-03 10:26:04 -0500

    Change questions to use db

--- legendarypotato/static/js/endless.js
+++ legendarypotato/static/js/endless.js
@@ -1,1 +1,17 @@
 var guessButton = document.getElementById('guess-button');
+var qid = document.getElementById('question-id').innerText;
+
+console.log(qid);
+
+guessButton.addEventListener('click', function() {
+  $.ajax({
+    type: "POST",
+    url: "/getquestion",
+    data: {
+      questionid: qid
+    },
+    success: function(e) {
+      console.log(e);
+    },
+  });
+});
