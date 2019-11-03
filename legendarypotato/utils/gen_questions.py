671ef1522c42757dddfe8b2a9fd4449836304b37
Hendrick Ducasse <hd1867@rit.edu>
2019-11-03 11:42:39 -0500

    added algebra generation

--- legendarypotato/utils/gen_questions.py
+++ legendarypotato/utils/gen_questions.py
@@ -1,15 +1,17 @@
 import database_utils
 import equation_solver
 import arith
+import alg
 """
 Generate a list of initial questions for the game
 -5 arith(easy)
 -5 arith(med)
 -5 arith(hard)
+-5 alg(beginner)
 """
 
 def init_quests():
-    for i in range(5):
+    for i in range(10):
         subject = 'arithmetic_basic'
         temp_quest = arith.make_arith_basic(3)[0]
         eqn = equation_solver.getRes(temp_quest)
@@ -43,6 +45,36 @@ def init_quests():
         database_utils.create_question(question_img, acceptable_answers, answer_img, subject)
 
         print("Created Advanced Arithmetic Question ", i)
+
+        subject = 'algebra_beginner'
+        temp_quest = alg.make_alg(0)
+        eqn = equation_solver.getRes(temp_quest)
+        question_img = eqn['input_img']
+        answer_img = eqn['answer_img']
+        acceptable_answers = eqn['acceptable_answers']
+
+        database_utils.create_question(question_img, acceptable_answers, answer_img, subject)
+        print("Created Beginner Algebra Question ", i)
+
+        subject = 'algebra_intermediate'
+        temp_quest = alg.make_alg(1)
+        eqn = equation_solver.getRes(temp_quest)
+        question_img = eqn['input_img']
+        answer_img = eqn['answer_img']
+        acceptable_answers = eqn['acceptable_answers']
+
+        database_utils.create_question(question_img, acceptable_answers, answer_img, subject)
+        print("Created Intermediate Algebra Question ", i)
+
+        subject = 'algebra_advanced'
+        temp_quest = alg.make_alg(2)
+        eqn = equation_solver.getRes(temp_quest)
+        question_img = eqn['input_img']
+        answer_img = eqn['answer_img']
+        acceptable_answers = eqn['acceptable_answers']
+
+        database_utils.create_question(question_img, acceptable_answers, answer_img, subject)
+        print("Created Advanced Algebra Question ", i)
 
 
 init_quests()
