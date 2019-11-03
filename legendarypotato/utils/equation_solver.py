2c4fd2c02f3a7fc8b5ec35fec7b062b6a85a73a1
Hendrick Ducasse <hd1867@rit.edu>
2019-11-03 01:50:06 -0500

    Fixed addition being multiplication

--- legendarypotato/utils/equation_solver.py
+++ legendarypotato/utils/equation_solver.py
@@ -11,39 +11,40 @@ def getRes(input):
     appid = '6QYPX8-HUAE3WKH7J'
     input = 'solve'+input
     answer = {}
-    queryStr = server + 'appid=' + appid + '&input=solve' + input + '&podstate=Result__Step-by-step+solution'
+    queryStr = server + 'appid=' + appid + '&podstate=Result__Step-by-step+solution'
 
     #print(requests.api.get(queryStr).content)
-    results = ET.fromstring((requests.api.get(queryStr).content))
+    results = ET.fromstring((requests.api.get(queryStr, params= {'input' : input} ).content))
     if results.attrib['success'] != 'false':
 
         for child in results:
             #print(child.attrib)
-            if child.attrib['title'] == 'Input interpretation' or child.attrib['title'] == 'Input':
-                for child2 in child:
+            if 'title' in child.attrib.keys():
+                if child.attrib['title'] == 'Input interpretation' or child.attrib['title'] == 'Input':
+                    for child2 in child:
 
-                    if str(child2.tag) == 'subpod':
+                        if str(child2.tag) == 'subpod':
+                            for child3 in child2:
+                                if child3.tag == 'img':
+                                    answer[child.attrib['title'] + '_img'] = child3.attrib['src']
+                                else:
+                                    answer[child.attrib['title'] + '_text'] = child3.text
+                if child.attrib['title'] == 'Results' or child.attrib['title'] == 'Exact result' or child.attrib['title'] == 'Decimal form':
+                    for child2 in child:
+
+                        if str(child2.tag) == 'subpod':
+                            for child3 in child2:
+                                if child3.tag == 'img':
+                                    answer[child.attrib['title'] + '_img'] = child3.attrib['src']
+                                else:
+                                    answer[child.attrib['title'] + '_text'] = child3.text
+                if "Reference" in child.attrib['title'] or "Exact Solution" in child.attrib['title'] or "Plot" in child.attrib['title']:
+                    for child2 in child:
                         for child3 in child2:
+                            if child3.tag == 'plaintext':
+                                answer[child.attrib['title'] + '_text'] = child3.text
                             if child3.tag == 'img':
                                 answer[child.attrib['title'] + '_img'] = child3.attrib['src']
-                            else:
-                                answer[child.attrib['title'] + '_text'] = child3.text
-            if child.attrib['title'] == 'Results' or child.attrib['title'] == 'Exact result' or child.attrib['title'] == 'Decimal form':
-                for child2 in child:
-
-                    if str(child2.tag) == 'subpod':
-                        for child3 in child2:
-                            if child3.tag == 'img':
-                                answer[child.attrib['title'] + '_img'] = child3.attrib['src']
-                            else:
-                                answer[child.attrib['title'] + '_text'] = child3.text
-            if "Reference" in child.attrib['title'] or "Exact Solution" in child.attrib['title'] or "Plot" in child.attrib['title']:
-                for child2 in child:
-                    for child3 in child2:
-                        if child3.tag == 'plaintext':
-                            answer[child.attrib['title'] + '_text'] = child3.text
-                        if child3.tag == 'img':
-                            answer[child.attrib['title'] + '_img'] = child3.attrib['src']
     else:
         print("The equation cannot be solved as written")
         for child in results:
@@ -51,3 +52,5 @@ def getRes(input):
                 print("did you mean: " + child2.text + " ?")
 
     return answer
+
+print(getRes("2x - 3 + 8y = 87"))
